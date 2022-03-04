784c01ca3c0b9b8775894ade4550a15c1b21311d
Edi <selimi.edi.1991@gmail.com>
2022-03-04 14:58:42 +0100

    CoinList Class to Const

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { NavBar } from "components/exports";
 import { CoinList, Portfolio, Coin } from "pages/exports";
@@ -8,6 +9,15 @@ class App extends React.Component {
   state = {
     currency: "USD",
     icon: "$",
+    data: [],
+    btcChartsData: [],
+    btcCurrentPrice: 0,
+    btcCurrentVolume: 0,
+    coinPrice: [],
+    coinVolumes: [],
+    coinTimestamp: [],
+    days: 7,
+    daysOptions: [1, 7, 30, 180, 365],
     // coin: "",
     theme: darkTheme,
   };
@@ -15,6 +25,12 @@ class App extends React.Component {
   updateCurrency = (newCurrency) => {
     if (this.state.currency !== newCurrency) {
       this.setState({ currency: newCurrency });
+    }
+  };
+
+  updateDays = (newDaysNumber) => {
+    if (this.state.days !== newDaysNumber) {
+      this.setState({ days: newDaysNumber });
     }
   };
 
@@ -31,6 +47,67 @@ class App extends React.Component {
       this.setState({ theme: darkTheme });
     }
   };
+
+  getCoinList = async () => {
+    try {
+      const { data } = await axios.get(
+        `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${this.state.currency}&order=market_cap_desc&per_page=1000&page=1&sparkline=true&price_change_percentage=1h,24h,7d`
+      );
+
+      this.setState({
+        data: data,
+        btcCurrentPrice: data[0].current_price,
+        btcCurrentVolume: data[0].total_volume,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  getBitcoinData = async () => {
+    try {
+      const btcData = await axios.get(
+        `https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=${this.state.currency}&days=${this.state.days}`
+      );
+      const volume = btcData.data.total_volumes.map((value, index) => {
+        let vol = value[1];
+        return vol;
+      });
+      const price = btcData.data.prices.map((value, index) => {
+        let priceValue = value[1];
+        return priceValue;
+      });
+      const date = btcData.data.prices.map((value, index) => {
+        let findChartDates = new Date(value[0]);
+        let chartDate = `${findChartDates.getDate()} - ${
+          findChartDates.getMonth() + 1
+        } - ${findChartDates.getFullYear()}`;
+        return chartDate;
+      });
+      this.setState({
+        coinPrice: price,
+        coinVolumes: volume,
+        coinTimestamp: date,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  componentDidMount = () => {
+    this.getCoinList();
+    this.getBitcoinData();
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (
+      prevState.currency !== this.state.currency ||
+      prevState.days !== this.state.days ||
+      prevProps.theme !== this.props.theme
+    ) {
+      this.getCoinList();
+      this.getBitcoinData();
+    }
+  }
 
   render() {
     const { currency, coin, icon, theme } = this.state;
@@ -50,7 +127,6 @@ class App extends React.Component {
               icon={icon}
               updateCurrency={this.updateCurrency}
               updateIcon={this.updateIcon}
-              // updateCoin={this.updateCoin}
               changeTheme={this.changeTheme}
               theme={theme}
             />
@@ -59,7 +135,11 @@ class App extends React.Component {
                 exact
                 path="/"
                 render={() => (
-                  <CoinList currency={currency} icon={icon} theme={theme} />
+                  <CoinList
+                    dataCoinList={this.state}
+                    updateDays={this.updateDays}
+                    data={this.state.data}
+                  />
                 )}
               />
               <Route exact path="/portfolio" component={Portfolio} />
--- src/pages/CoinList/index.js
+++ src/pages/CoinList/index.js
@@ -1,5 +1,4 @@
-import React from "react";
-import axios from "axios";
+import React, { useState } from "react";
 import { LineChart, BarChart, SelectDays, Table } from "components/exports";
 import {
   PageContainer,
@@ -12,161 +11,79 @@ import { currentDate } from "utils/date.js";
 import { nFormatter } from "utils/nFormatter";
 import { ThemeProvider } from "styled-components";
 
-class CoinList extends React.Component {
-  state = {
-    data: [],
-    sortedDesc: true,
-    btcChartsData: [],
-    btcCurrentPrice: 0,
-    btcCurrentVolume: 0,
-    days: 7,
-    daysOptions: [1, 7, 30, 180, 365],
-    coinPrice: [],
-    coinVolumes: [],
-    coinTimestamp: [],
-  };
+const CoinList = ({ dataCoinList, data, updateDays }) => {
+  const [sortedDesc, setSortDesc] = useState(true);
+  const setDays = (daysNumber) => updateDays(daysNumber);
 
-  //corsProblemFixer = "https://cors-anywhere.herokuapp.com/";
-
-  getCoinList = async () => {
-    try {
-      const { data } = await axios.get(
-        `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${this.props.currency}&order=market_cap_desc&per_page=1000&page=1&sparkline=true&price_change_percentage=1h,24h,7d`
-      );
-
-      this.setState({
-        data: data,
-        btcCurrentPrice: data[0].current_price,
-        btcCurrentVolume: data[0].total_volume,
-      });
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  getBitcoinData = async () => {
-    try {
-      const btcData = await axios.get(
-        `https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=${this.props.currency}&days=${this.state.days}`
-      );
-      const volume = btcData.data.total_volumes.map((value, index) => {
-        let vol = value[1];
-        return vol;
-      });
-      const price = btcData.data.prices.map((value, index) => {
-        let priceValue = value[1];
-        return priceValue;
-      });
-      const date = btcData.data.prices.map((value, index) => {
-        let findChartDates = new Date(value[0]);
-        let chartDate = `${findChartDates.getDate()} - ${
-          findChartDates.getMonth() + 1
-        } - ${findChartDates.getFullYear()}`;
-        return chartDate;
-      });
-      this.setState({
-        coinPrice: price,
-        coinVolumes: volume,
-        coinTimestamp: date,
-      });
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  setDays = (daysNumber) => {
-    this.setState({ days: daysNumber });
-  };
-
-  sortByAscOrDesc = () => {
-    if (this.state.sortedDesc === true) {
-      let sortedAsceding = this.state.data.sort((a, b) => {
+  const sortByAscOrDesc = () => {
+    if (sortedDesc === true) {
+      let sortedAsceding = data.sort((a, b) => {
         return a.market_cap - b.market_cap;
       });
-      this.setState({ sortedDesc: false });
+      setSortDesc(false);
       return sortedAsceding;
-    } else if (this.state.sortedDesc === false) {
-      let sortedDesceding = this.state.data.sort((a, b) => {
+    } else if (sortedDesc === false) {
+      let sortedDesceding = data.sort((a, b) => {
         return b.market_cap - a.market_cap;
       });
-      this.setState({ sortedDesc: true });
+      setSortDesc(true);
       return sortedDesceding;
     }
   };
 
-  componentDidMount = () => {
-    this.getCoinList();
-    this.getBitcoinData();
-  };
+  const {
+    btcCurrentPrice,
+    coinPrice,
+    icon,
+    coinTimestamp,
+    btcCurrentVolume,
+    coinVolumes,
+    daysOptions,
+    theme,
+  } = dataCoinList;
+  return (
+    <ThemeProvider theme={theme}>
+      <PageContainer>
+        <MainContainer>
+          <h4>Your overview</h4>
+          <ChartsContainer>
+            <ChartContainer>
+              <h6>BTC</h6>
+              <h2>
+                {btcCurrentPrice}&nbsp;
+                {icon}
+              </h2>
+              <h6>{currentDate}</h6>
+              <LineChart coinPrice={coinPrice} coinTimestamp={coinTimestamp} />
+            </ChartContainer>
+            <ChartContainer>
+              <h6>Volume 24H</h6>
+              <h2>{nFormatter(btcCurrentVolume)}</h2>
+              <h6>{currentDate}</h6>
+              <BarChart
+                coinTotalVolumes={coinVolumes}
+                coinTimestamp={coinTimestamp}
+                width={300}
+              />
+            </ChartContainer>
+          </ChartsContainer>
+          <SelectDays
+            days={daysOptions}
+            selectNumberOfDays={(number) => setDays(number)}
+            theme={theme}
+          />
+          <h4>Your overview</h4>
+          <Table
+            data={data}
+            icon={icon}
+            orderList={sortedDesc}
+            orderCoinList={sortByAscOrDesc}
+            theme={theme}
+          />
+        </MainContainer>
+      </PageContainer>
+    </ThemeProvider>
+  );
+};
 
-  componentDidUpdate(prevProps, prevState) {
-    if (
-      prevProps.currency !== this.props.currency ||
-      prevState.days !== this.state.days ||
-      prevProps.theme !== this.props.theme
-    ) {
-      this.getCoinList();
-      this.getBitcoinData();
-    }
-  }
-
-  render() {
-    const {
-      btcCurrentPrice,
-      coinPrice,
-      coinTimestamp,
-      btcCurrentVolume,
-      coinVolumes,
-      daysOptions,
-      data,
-      sortedDesc,
-    } = this.state;
-    return (
-      <ThemeProvider theme={this.props.theme}>
-        <PageContainer>
-          <MainContainer>
-            <h4>Your overview</h4>
-            <ChartsContainer>
-              <ChartContainer>
-                <h6>BTC</h6>
-                <h2>
-                  {btcCurrentPrice}&nbsp;
-                  {this.props.icon}
-                </h2>
-                <h6>{currentDate}</h6>
-                <LineChart
-                  coinPrice={coinPrice}
-                  coinTimestamp={coinTimestamp}
-                />
-              </ChartContainer>
-              <ChartContainer>
-                <h6>Volume 24H</h6>
-                <h2>{nFormatter(btcCurrentVolume)}</h2>
-                <h6>{currentDate}</h6>
-                <BarChart
-                  coinTotalVolumes={coinVolumes}
-                  coinTimestamp={coinTimestamp}
-                  width={300}
-                />
-              </ChartContainer>
-            </ChartsContainer>
-            <SelectDays
-              days={daysOptions}
-              selectNumberOfDays={(number) => this.setDays(number)}
-              theme={this.props.theme}
-            />
-            <h4>Your overview</h4>
-            <Table
-              data={data}
-              icon={this.props.icon}
-              orderList={sortedDesc}
-              orderCoinList={this.sortByAscOrDesc}
-              theme={this.props.theme}
-            />
-          </MainContainer>
-        </PageContainer>
-      </ThemeProvider>
-    );
-  }
-}
 export default CoinList;
